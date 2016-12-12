1eadae2c86e996520bd95d67483e588dbc74efc6
deanturpin <git@lvbtn.uk>
2016-12-12 19:56:00 +0000

    consolidate canvas dimension defs

--- geometry.js
+++ geometry.js
@@ -4,20 +4,18 @@
 onload = function() {
 
 	// Create a canvas element
-	var canvas = document.createElement("canvas")
-	var body = document.getElementsByTagName("body")[0]
+	const canvas = document.createElement("canvas")
+	const body = document.getElementsByTagName("body")[0]
 
 	// Add it to the DOM
 	body.appendChild(canvas)
 
 	// Define canvas (view port)
-	var context = canvas.getContext("2d")
+	const context = canvas.getContext("2d")
 
 	// Set canvas size
-	const width = 400 // window.innerWidth
-	const height = 400 // window.innerHeight
-	canvas.width = width
-	canvas.height = height
+	const width = canvas.width = window.innerWidth * .9
+	const height = canvas.height = window.innerHeight * .8
 
 	// Create pixel
 	const pixel = context.createImageData(1, 1)
@@ -28,14 +26,19 @@ onload = function() {
 	pixel.data[2] = 0
 	pixel.data[3] = 255
 
+	// X axis offset
+	var offset = 0
 
-	var offset = 1
+	// Start animation
+	requestAnimationFrame(draw)
 
+	// Draw a single frame
 	function draw() {
 
 		// Clear the canvas
-		context.clearRect(0, 0, canvas.width, canvas.height)
+		context.clearRect(0, 0, width, height)
 
+		// Plot the shape
 		for (var x = 0; x < width; ++x) {
 
 			const y = Math.floor(height/2
@@ -44,12 +47,12 @@ onload = function() {
 			context.putImageData(pixel, x, y)
 		}
 
+		// Move along the X axis
 		++offset
 
+		// Get next frame
 		requestAnimationFrame(draw)
 	}
-
-	requestAnimationFrame(draw)
 
 	// Periodically reload the page
 	setInterval(function() {
